aa45e2e4a36125ae11f3b754aad2247792fe4d1a
OshkarVTec <100952154+OshkarVTec@users.noreply.github.com>
2022-06-09 10:54:38 -0500

    Add files via upload

--- retoOLED.py
+++ retoOLED.py
@@ -3,7 +3,7 @@ import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 from pathlib import Path
-import os
+from demo_opts import get_device
 i2c = busio.I2C(SCL, SDA)
 disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 disp.fill(0)
@@ -11,17 +11,96 @@ disp.show()
 image = Image.new('1', (128, 64))
 draw = ImageDraw.Draw(image)
 
-
-
-def showInfo(name, artist, album):
+def showInfo(number, name, artist, album):
     fontSize2= 15
     fontSize1= 10
     font1 = ImageFont.truetype("/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf",fontSize1)
     font2 = ImageFont.truetype("/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf",fontSize2)
-    if len(name) > 19:
+    if len(name) > 17:
         font2 = font1
-    draw.text((1, 10), name, font = font2, fill = 255)
+    title = str(number) + " " + name
+    draw.text((1, 10), title, font = font2, fill = 255)
     draw.text((1, 35), artist, font = font1, fill = 255)
     draw.text((1, 50), album, font = font1, fill = 255)
     disp.image(image)
     disp.show()
+
+def showNumber(number):
+    disp.fill(0)
+    disp.show()
+    fontSize1= 40
+    font1 = ImageFont.truetype("/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf",fontSize1)
+    draw.text((40, 10), str(number), font = font1, fill = 255)
+    disp.image(image)
+    disp.show()
+
+def randomOled():
+    device = get_device()
+    img_path = str(Path(__file__).resolve().parent.joinpath('images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    background.paste(logo, posn)
+    for i in range(10):
+        device.display(background.convert(device.mode))
+
+def continuosOled():
+    device = get_device()
+    img_path = str(Path(__file__).resolve().parent.joinpath('images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    background.paste(logo, posn)
+    for i in range(10):
+        device.display(background.convert(device.mode))
+
+def playOled():
+    device = get_device()
+    img_path = str(Path(__file__).resolve().parent.joinpath('images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    background.paste(logo, posn)
+    for i in range(10):
+        device.display(background.convert(device.mode))
+
+def pauseOled():
+    device = get_device()
+    img_path = str(Path(__file__).resolve().parent.joinpath('images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    background.paste(logo, posn)
+    for i in range(10):
+        device.display(background.convert(device.mode))
+
+def nextOled():
+    device = get_device()
+    img_path = str(Path(__file__).resolve().parent.joinpath('images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    background.paste(logo, posn)
+    for i in range(10):
+        device.display(background.convert(device.mode))
+
+def previousOled():
+    device = get_device()
+    img_path = str(Path(__file__).resolve().parent.joinpath('images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    background.paste(logo, posn)
+    for i in range(10):
+        device.display(background.convert(device.mode))
+
+randomOled()
+
+showInfo(11,"La Corriente", "Bad Bunny", "Un verano sin ti")
+showNumber(11)
