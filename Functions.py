dee6ca1f1e197a5782db6ceaf18ee87644e94a6d
Diego <A01735217@tec.mx>
2022-06-10 12:01:39 -0500

    Added random files

---
+++ Functions.py
@@ -0,0 +1,35 @@
+import os, pygame
+import re
+import random
+from PyQt5 import QtGui, QtCore
+
+from pygame import mixer
+
+pygame.mixer.init()
+if not pygame.font: print ('Warning, fonts disabled')
+if not pygame.mixer: print ('Warning, sound disabled')
+
+def play(songPath, isPaused):
+   if (isPaused):
+      pygame.mixer.unpause()
+   else:
+      pygame.mixer.music.load(songPath)
+      pygame.mixer.music.play()
+   isPaused = False
+   return isPaused
+
+def pause(isPaused):
+   pygame.mixer.music.pause()
+   isPaused = True
+   return isPaused
+
+
+
+
+
+
+
+
+
+
+
