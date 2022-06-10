f448393cc00916a9aa0b4178037aec1a84e96253
OshkarVTec <a01275287@tec.mx>
2022-06-10 11:45:38 -0500

    .

--- Spotify.py
+++ Spotify.py
@@ -248,13 +248,3 @@ if __name__ == "__main__":
     sys.exit(app.exec_())
 
 
-
-#def getSongs():
- #   currentPath = os.path.dirname(os.path.abspath(__file__)); # Absolute dir the script is in
-  #  filepath = "..\songs\ "; # The path where the pictures are uploaded
-   # directory = os.listdir(os.path.join(currentPath, filepath));
-    #songs = [ fi for fi in directory if fi.endswith(('.mp3')) ];
-    #return songs;
-
-#songs = getSongs()
-#print (songs)
