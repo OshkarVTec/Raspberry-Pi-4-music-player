0e3fc97ca500f4000a44df4aec16b152e2085b63
OshkarVTec <a01275287@tec.mx>
2022-06-10 12:53:43 -0500

    Implementation progress

--- ImplementacionSpotify.py
+++ ImplementacionSpotify.py
@@ -29,28 +29,28 @@ class Ui_Dialog(QtWidgets.QMainWindow,Ui_MainWindow):
         self.isPaused = False
         self.setupUi(self)
         
-        self.button_FF.clicked.connect(self.nextPressed(False))
-        self.button_Play.clicked.connect(self.playPressed(False))
-        self.button_Random.clicked.connect(self.randomPressed(False))
-        self.button_Repeat.clicked.connect(self.repeatPressed(False))
-        self.button_Rewind.clicked.connect(self.rewindPressed(False))
+        self.button_FF.clicked.connect(lambda : self.nextPressed())
+        self.button_Play.clicked.connect(lambda :self.playPressed(False))
+        self.button_Random.clicked.connect(lambda : self.randomPressed(False))
+        self.button_Repeat.clicked.connect(lambda : self.repeatPressed(False))
+        self.button_Rewind.clicked.connect(lambda : self.rewindPressed(False))
 
     def nextPressed(self, gui):
         if self.random == True:
             self.current = random.randint(0,99)
         else: 
-            self.current =+ 1
+            self.current += 1
         self.button_Play.setChecked(True)
         retoOLED.nextOled()
         self.playPressed(False)
 
     def playPressed(self, gui):
-        if self.button_Play.isClicked() == False:
+        if self.button_Play.isChecked() == False:
             retoOLED.playOled()
             self.isPaused = Functions.play(self.songs[self.current], self.isPaused)
         else:
             retoOLED.pauseOled()
-            self.isPaused = Functions.pause(self.songs[self.current], self.isPaused)
+            self.isPaused = Functions.pause(self.isPaused)
         
     def randomPressed(self, gui):
         retoOLED.randomOled()
@@ -61,6 +61,15 @@ class Ui_Dialog(QtWidgets.QMainWindow,Ui_MainWindow):
         retoOLED.repeatOled()
         self.repeat = not self.repeat
         
+    def rewindPressed(self, gui):
+        if self.random == True:
+            self.current = random.randint(0,99)
+        else: 
+            self.current -= 1
+        self.button_Play.setChecked(True)
+        retoOLED.nextOled()
+        self.playPressed(False)
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = Ui_Dialog()
