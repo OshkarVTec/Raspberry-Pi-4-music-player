638f03d5cf901f1a4aadc0df9ba9f204221e876c
OshkarVTec <a01275287@tec.mx>
2022-06-10 15:12:58 -0500

    .

--- ImplementacionSpotify.py
+++ ImplementacionSpotify.py
@@ -41,24 +41,24 @@ class Ui_Dialog(QtWidgets.QMainWindow,Ui_MainWindow):
         else: 
             self.current += 1
         self.button_Play.setChecked(True)
-        retoOLED.nextOled()
+        #retoOLED.nextOled()
         self.isPaused = Functions.play(self.songs[self.current], self.isPaused)
 
     def playPressed(self, gui):
         if self.button_Play.isChecked() == False:
-            retoOLED.playOled()
+            #retoOLED.playOled()
             self.isPaused = Functions.play(self.songs[self.current], self.isPaused)
         else:
-            retoOLED.pauseOled()
+            #retoOLED.pauseOled()
             self.isPaused = Functions.pause(self.isPaused)
         
     def randomPressed(self, gui):
-        retoOLED.randomOled()
+        #retoOLED.randomOled()
         self.random = not self.random
         self.current = random.randint(0,99)
 
     def repeatPressed(self, gui):
-        retoOLED.repeatOled()
+        #retoOLED.repeatOled()
         self.repeat = not self.repeat
         
     def rewindPressed(self, gui):
@@ -67,7 +67,7 @@ class Ui_Dialog(QtWidgets.QMainWindow,Ui_MainWindow):
         else: 
             self.current -= 1
         self.button_Play.setChecked(True)
-        retoOLED.nextOled()
+        #retoOLED.nextOled()
         self.isPaused = Functions.play(self.songs[self.current], self.isPaused)
 
 if __name__ == "__main__":
