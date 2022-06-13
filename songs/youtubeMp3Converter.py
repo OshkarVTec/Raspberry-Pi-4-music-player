9e651d4b8ca291f69cac13ed0bdb916fe1f23107
OshkarVTec <a01275287@tec.mx>
2022-06-13 08:18:31 -0500

    Added songs and covers

---
+++ songs/youtubeMp3Converter.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+import eyed3
+import youtube_dl
+
+songs = [
+'https://www.youtube.com/watch?v=2NiyrtYegso&list=PLm87MSayQYPoUsizXajFTBtx05kYZXXYR'
+] #Links de la playlist oficial
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.',
+    'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        },
+        {
+            'key': 'FFmpegMetadata'
+        }]
+   
+}
+for i in songs:
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([i])
+        ydl.cache.remove()
